f61a3efe08810079fead6218f3c1bacdfe12cfe9
shccgxqp <shccgxqp@gmail.com>
2023-11-23 14:55:08 +0800

    修正資料夾路徑

--- function/us_stock.py
+++ function/us_stock.py
@@ -5,6 +5,7 @@ import pandas as pd
 import mplfinance as mpf
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+from function.translations import translation_dict
 
 def get_intraDay_data_today(stock_symbol='AAPL'):
     """
@@ -112,7 +113,8 @@ def download_financial_data(stk_list):
     failed_list = []
 
     # 創建以今天日期為名的資料夾，如果不存在的話
-    base_folder = os.path.join(os.getcwd(), today_date)
+    base_folder = os.path.join(os.getcwd(),'Data', today_date)
+
     os.makedirs(base_folder, exist_ok=True)
 
     # 開始迴圈抓資料囉！
@@ -127,9 +129,9 @@ def download_financial_data(stk_list):
 
             # 填入股票代碼後直接下載損益表、資產負債表和現金流量表成 CSV 格式
             stock = yf.Ticker(i)
-            stock.financials.to_csv(os.path.join(stock_folder, f'損益表_{i}.csv'))
-            stock.balance_sheet.to_csv(os.path.join(stock_folder, f'資產負債表_{i}.csv'))
-            stock.cashflow.to_csv(os.path.join(stock_folder, f'現金流量表_{i}.csv'))
+            stock.financials.to_csv(os.path.join(stock_folder, f'損益表.csv'))
+            stock.balance_sheet.to_csv(os.path.join(stock_folder, f'資產負債表.csv'))
+            stock.cashflow.to_csv(os.path.join(stock_folder, f'現金流量表.csv'))
 
             # 停一秒，再抓下一檔，避免對伺服器造成負擔而被鎖住
             time.sleep(1)
@@ -189,67 +191,9 @@ def translate_and_save(csv_path='/', output_suffix='_tw'):
     - None
     """
 
-    translation_dict={
-    'Beginning Cash Position': '期初現金餘額',
-    'Capital Expenditure': '資本支出',
-    'Cash Dividends Paid': '支付現金股利',
-    'Cash Flow From Continuing Financing Activities': '來自持續融資活動的現金流量',
-    'Cash Flow From Continuing Investing Activities': '來自持續投資活動的現金流量',
-    'Cash Flow From Continuing Operating Activities': '來自持續營運活動的現金流量',
-    'Change In Account Payable': '應付賬款的變動',
-    'Change In Inventory': '存貨的變動',
-    'Change In Other Current Assets': '其他流動資產的變動',
-    'Change In Other Current Liabilities': '其他流動負債的變動',
-    'Change In Other Working Capital': '其他營運資本的變動',
-    'Change In Payable': '應付款項的變動',
-    'Change In Payables And Accrued Expense': '應付款項及應計費用的變動',
-    'Change In Receivables': '應收款項的變動',
-    'Change In Working Capital': '營運資本的變動',
-    'Changes In Account Receivables': '應收賬款的變動',
-    'Changes In Cash': '現金變動',
-    'Common Stock Dividend Paid': '支付普通股股息',
-    'Common Stock Issuance': '發行普通股',
-    'Common Stock Payments': '支付普通股款',
-    'Deferred Income Tax': '递延所得税',
-    'Deferred Tax': '递延税款',
-    'Depreciation Amortization Depletion': '折舊、攤銷和減值',
-    'Depreciation And Amortization': '折舊和攤銷',
-    'End Cash Position': '期末現金餘額',
-    'Financing Cash Flow': '融資現金流',
-    'Free Cash Flow': '自由現金流',
-    'Income Tax Paid Supplemental Data': '所得税支付（附加数据）',
-    'Interest Paid Supplemental Data': '支付利息（附加数据）',
-    'Investing Cash Flow': '投資現金流',
-    'Issuance Of Capital Stock': '發行股本',
-    'Issuance Of Debt': '發行債務',
-    'Long Term Debt Issuance': '長期債務發行',
-    'Long Term Debt Payments': '長期債務支付',
-    'Net Business Purchase And Sale': '淨業務購買和銷售',
-    'Net Common Stock Issuance': '淨普通股發行',
-    'Net Income From Continuing Operations': '持續營運的淨收入',
-    'Net Investment Purchase And Sale': '淨投資購買和銷售',
-    'Net Issuance Payments Of Debt': '債務的淨發行和支付',
-    'Net Long Term Debt Issuance': '淨長期債務發行',
-    'Net Other Financing Charges': '融資的其他淨費用',
-    'Net Other Investing Changes': '投資的其他淨變動',
-    'Net PPE Purchase And Sale': '固定資產和無形資產的淨購買和銷售',
-    'Net Short Term Debt Issuance': '淨短期債務發行',
-    'Operating Cash Flow': '營運現金流',
-    'Other Non Cash Items': '其他非現金項目',
-    'Purchase Of Business': '購併企業',
-    'Purchase Of Investment': '購買投資',
-    'Purchase Of PPE': '購買固定資產和無形資產',
-    'Repayment Of Debt': '債務的還款',
-    'Repurchase Of Capital Stock': '回購股',
-    'Sale Of Investment' : '投資賣出',
-    'Short Term Debt Issuance' : '短期債務的發行',
-    'Short Term Debt Payments' : '短期債務的支付',
-    'Stock Based Compensation' : '股票基於報酬',
-    }
-
     # 讀取 CSV 文件
     df = pd.read_csv(csv_path)
-
+    print(translation_dict)
     # 修改所有行的第一列
     df.iloc[:, 0] = df.iloc[:, 0].map(translation_dict.get)
 
@@ -261,5 +205,6 @@ def translate_and_save(csv_path='/', output_suffix='_tw'):
     # 將修改後的 DataFrame 寫入 CSV 文件
     df.to_csv(output_path, index=False, encoding='utf-8')
 
-def test():
+
+def main2():
     return True
