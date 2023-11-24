6728b1710489d2e547290707be566ea1fb99c68d
shccgxqp <shccgxqp@gmail.com>
2023-11-24 17:34:19 +0800

    設定環境變數 & 資料夾整理

--- backend_django/manage.py
+++ backend_django/manage.py
@@ -2,11 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from backend_django.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend_django.settings")
+
+    if base.DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend_django.settings.local")
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "backend_django.settings.production"
+        )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
